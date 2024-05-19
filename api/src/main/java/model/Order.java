49665a382a878a15202793eb1ba720179f470739
Nguyen Ngoc Quy <21130506@st.hcmuaf.edu.vn>
2024-05-19 20:42:47 +0700

    add server demo

---
+++ api/src/main/java/model/Order.java
@@ -0,0 +1,23 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private int id;
+    private int userId;
+    private double totalPrice;
+    private Timestamp orderDate;
+    private String status;
+    private String consigneeName;
+    private String consigneePhoneNumber;
+    private String address;
+    private double shippingFee;
+    private String note;
+}
