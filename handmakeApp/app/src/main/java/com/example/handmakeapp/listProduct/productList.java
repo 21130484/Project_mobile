c12a5dd543964ba0f537dd8875305174dce0c168
NTNBackEnd <nguyennghia163tp@gmail.com>
2024-05-26 10:19:44 +0700

    update filter in productList

--- handmakeApp/app/src/main/java/com/example/handmakeapp/listProduct/productList.java
+++ handmakeApp/app/src/main/java/com/example/handmakeapp/listProduct/productList.java
@@ -1,8 +1,11 @@
 package com.example.handmakeapp.listProduct;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,7 @@ import com.example.handmakeapp.R;
 public class productList extends AppCompatActivity {
      ListView listProduct1;
     ListView listProduct2;
+    Spinner filterProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,19 @@ public class productList extends AppCompatActivity {
         String[] arr2 = {"dép","giày","mũ","ống tay","bó gối"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arr2);
         listProduct2.setAdapter(adapter2);
+
+        String[] option = {"Khung Ảnh HandMade","Thiệp HandMade","Đồ Dùng HandMade"};
+        ArrayAdapter<String> adapterFilter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,option);
+        adapterFilter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterProduct = findViewById(R.id.filterProductList);
+        filterProduct.setAdapter(adapterFilter);
+//        filterProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String selectOption = option[position];
+//                System.out.println(selectOption);
+//            }
+//        });
+
     }
 }
