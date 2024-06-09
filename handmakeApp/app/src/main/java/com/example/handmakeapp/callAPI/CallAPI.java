d18c74374fd065d879fa29a584306c05dee391bd
Pblues <lungbaphe772003@gmail.com>
2024-06-09 13:52:31 +0700

    quamm

--- handmakeApp/app/src/main/java/com/example/handmakeapp/callAPI/CallAPI.java
+++ handmakeApp/app/src/main/java/com/example/handmakeapp/callAPI/CallAPI.java
@@ -25,7 +25,7 @@ import retrofit2.http.Query;
 
 public interface CallAPI {
 
-    public final static String SERVER_IP = "192.168.88.104".trim();//ipconfig
+    public final static String SERVER_IP = "192.168.88.252".trim();//ipconfig
     public final static String SERVER_PORT = "8080";
 
     public static String getAbsoluteURL() {
--- handmakeApp/app/src/main/java/com/example/handmakeapp/home_products/Home.java
+++ handmakeApp/app/src/main/java/com/example/handmakeapp/home_products/Home.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,11 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.handmakeapp.R;
-import com.example.handmakeapp.account.Account;
 import com.example.handmakeapp.callAPI.CallAPI;
 import com.example.handmakeapp.cartActivity;
 import com.example.handmakeapp.detail_product.DetailActivity;
 import com.example.handmakeapp.home_products.adapter.ProductListRecyclerViewAdapter;
+import com.example.handmakeapp.home_products.adapter.RecyclerItemClickListener;
 import com.example.handmakeapp.home_products.adapter.SlideAdapter;
 import com.example.handmakeapp.home_products.mapping.BannerMapping;
 import com.example.handmakeapp.home_products.mapping.ProductMapping;
@@ -50,6 +51,7 @@ public class Home extends AppCompatActivity {
     ViewPager2 viewPager2;
     List<BannerItem> bannerItems = new ArrayList<>();
     BottomNavigationView bottomNavigation;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,8 +81,6 @@ public class Home extends AppCompatActivity {
         viewPager2.setOffscreenPageLimit(3);
         viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
         actionNavigationBottom();
-
-        
     }
 
     private class NetworkTask extends AsyncTask<Void, Void, List<Product>> {
@@ -101,10 +101,14 @@ public class Home extends AppCompatActivity {
             rv_1.setAdapter(adapter);
             rv_2.setAdapter(adapter_2);
 
+            addListenerForRecyclerView(rv_1, topSoldoutProducts);
+            addListenerForRecyclerView(rv_2, discountProducts);
+
             SlideAdapter slideAdapter = new SlideAdapter(bannerItems, viewPager2);
             viewPager2.setAdapter(slideAdapter);
         }
     }
+
     public void actionNavigationBottom() {
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setSelectedItemId(R.id.home);
@@ -113,32 +117,33 @@ public class Home extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
-                if (id == R.id.home) {
+                if (id == R.id.account) {
                     return true;
-                } else if (id == R.id.account) {
-                    startActivity(new Intent(getApplicationContext(), Account.class));
-                    overridePendingTransition(0,0);
+                } else if (id == R.id.home) {
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                    overridePendingTransition(0, 0);
                     return true;
                 } else if (id == R.id.list) {
                     startActivity(new Intent(getApplicationContext(), Products.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
                 } else if (id == R.id.cart) {
                     startActivity(new Intent(getApplicationContext(), cartActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
                 }
                 return false;
             }
         });
     }
+
     private void getProductById(int id) {
         CallAPI.api.getPDById("getProductDetailsById", id).enqueue(new Callback<ProductDetail>() {
             @Override
             public void onResponse(Call<ProductDetail> call, Response<ProductDetail> response) {
                 Log.e("Kien", "onSuccess");
                 ProductDetail p = response.body();
-                if(p != null) {
+                if (p != null) {
                     Log.e("Kien", p.toString());
 
                     //Chuyển sang DetailActivity truyền ProductDetail.
@@ -146,16 +151,33 @@ public class Home extends AppCompatActivity {
                     intent.putExtra("productDetail", p);
                     startActivity(intent);
 
-                }
-
-                else {
-                    Log.e("Kien", response.code()+ "");
+                } else {
+                    Log.e("Kien", response.code() + "");
                 }
             }
+
             @Override
             public void onFailure(Call<ProductDetail> call, Throwable t) {
                 Log.e("Kien", "Failure" + t.getMessage(), t);
             }
         });
+    }
+
+    public void addListenerForRecyclerView(RecyclerView recyclerView, List<Product> mapProducts) {
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+//                Toast.makeText(Home.this, "onItemClick", Toast.LENGTH_SHORT).show();
+                int idt = mapProducts.get(position).getId();
+                getProductById(idt);
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+//                Toast.makeText(Home.this, "onLongItemClick", Toast.LENGTH_SHORT).show();
+                int idt = mapProducts.get(position).getId();
+                getProductById(idt);
+            }
+        }));
     }
 }
