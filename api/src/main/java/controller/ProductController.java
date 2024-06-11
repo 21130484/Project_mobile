6a3832a06e40a7f2226f5a1c21c1331a81f7c18a
Ba Phung Le <108784632+pbluest-rong@users.noreply.github.com>
2024-06-11 13:07:18 +0700

    Update ProductController.java

--- api/src/main/java/controller/ProductController.java
+++ api/src/main/java/controller/ProductController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/api-product")
@@ -38,31 +39,62 @@ public class ProductController extends HttpServlet {
 
             if (action.equals("getAllProducts")) {
                 List<Product> products = productDAO.getAll();
-                objectMapper.writeValue(resp.getOutputStream(), products);
+                List<ProductDetail> productDetails = new ArrayList<>();
+                int id;
+                for (Product product : products) {
+                    id = product.getId();
+                    List<Image> imageList = i.getImageForPDetails(id);
+                    List<Rate> rateList = r.getRateByProduct(id);
+                    ProductDetail productDetail = p.getPDetailsById(id);
+                    productDetail.setImageList(imageList);
+                    productDetail.setRateList(rateList);
+                    productDetails.add(productDetail);
+                }
+                resp.setCharacterEncoding("UTF-8");
+                resp.setContentType("application/json");
+                objectMapper.writeValue(resp.getOutputStream(), productDetails);
             } else if (action.equals("getImageByProductId")) {
                 String productId = req.getParameter("productId");
                 if (productId != null) {
                     List<Image> images = imageDAO.getImagesForProduct(productId);
                     objectMapper.writeValue(resp.getOutputStream(), images);
                 }
-            } else if (action.equals("getProductById")) {
-                String productId = req.getParameter("productId");
-                if (productId != null) {
-                    Product products = productDAO.getProductById(productId);
-                    objectMapper.writeValue(resp.getOutputStream(), products);
-                }
             } else if (action.equals("getTopSoldoutProducts")) {
                 List<Product> products = productDAO.getTopSoldoutProduct(15);
-                objectMapper.writeValue(resp.getOutputStream(), products);
+                List<ProductDetail> productDetails = new ArrayList<>();
+                int id;
+                for (Product product : products) {
+                    id = product.getId();
+                    List<Image> imageList = i.getImageForPDetails(id);
+                    List<Rate> rateList = r.getRateByProduct(id);
+                    ProductDetail productDetail = p.getPDetailsById(id);
+                    productDetail.setImageList(imageList);
+                    productDetail.setRateList(rateList);
+                    productDetails.add(productDetail);
+                }
+                resp.setCharacterEncoding("UTF-8");
+                resp.setContentType("application/json");
+                objectMapper.writeValue(resp.getOutputStream(), productDetails);
             } else if (action.equals("getCategories")) {
                 List<Category> categories = productDAO.getCategories();
                 objectMapper.writeValue(resp.getOutputStream(), categories);
             } else if (action.equals("getDiscountProducts")) {
                 List<Product> products = productDAO.getDiscountProducts();
-                objectMapper.writeValue(resp.getOutputStream(), products);
+                List<ProductDetail> productDetails = new ArrayList<>();
+                int id;
+                for (Product product : products) {
+                    id = product.getId();
+                    List<Image> imageList = i.getImageForPDetails(id);
+                    List<Rate> rateList = r.getRateByProduct(id);
+                    ProductDetail productDetail = p.getPDetailsById(id);
+                    productDetail.setImageList(imageList);
+                    productDetail.setRateList(rateList);
+                    productDetails.add(productDetail);
+                }
+                resp.setCharacterEncoding("UTF-8");
+                resp.setContentType("application/json");
+                objectMapper.writeValue(resp.getOutputStream(), productDetails);
             } else if (action.equals("getProductDetailsById")) {
-
-
                 String pid = req.getParameter("productId");
                 int id = Integer.parseInt(pid);
                 List<Image> imageList = i.getImageForPDetails(id);
@@ -79,33 +111,32 @@ public class ProductController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            ObjectMapper objectMapper = new ObjectMapper();
-            req.setCharacterEncoding("UTF-8");
-            resp.setContentType("application/json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
 
-            String action = req.getParameter("action");
+        String action = req.getParameter("action");
 
-            if(action != null) {
-                if (action.equals("updateQuantity")) {
-                    int newQuantity = Integer.parseInt(req.getParameter("newQuantity"));
-                    int cartItemId = Integer.parseInt(req.getParameter("cartItemId"));
-                    int cartId = Integer.parseInt(req.getParameter("cartId"));
-                    boolean isUpdated = cartDAO.updateQuantity(newQuantity, cartItemId, cartId);
-                    objectMapper.writeValue(resp.getOutputStream(), isUpdated);
-                } else if (action.equals("addCartWithItems")) {
+        if (action != null) {
+            if (action.equals("updateQuantity")) {
+                int newQuantity = Integer.parseInt(req.getParameter("newQuantity"));
+                int cartItemId = Integer.parseInt(req.getParameter("cartItemId"));
+                int cartId = Integer.parseInt(req.getParameter("cartId"));
+                boolean isUpdated = cartDAO.updateQuantity(newQuantity, cartItemId, cartId);
+                objectMapper.writeValue(resp.getOutputStream(), isUpdated);
+            } else if (action.equals("addCartWithItems")) {
 
-                    String userId = req.getParameter("userId");
+                String userId = req.getParameter("userId");
 
-                    int productId = Integer.parseInt(req.getParameter("productId"));
-                    int quantity = Integer.parseInt(req.getParameter("quantity"));
-                    if(userId != null) {
-                        cartDAO.addCartWithItem(userId, productId, quantity);
-                        objectMapper.writeValue(resp.getOutputStream(), null);
-                    }
+                int productId = Integer.parseInt(req.getParameter("productId"));
+                int quantity = Integer.parseInt(req.getParameter("quantity"));
+                if (userId != null) {
+                    cartDAO.addCartWithItem(userId, productId, quantity);
+                    objectMapper.writeValue(resp.getOutputStream(), null);
                 }
-
             }
 
+        }
 
 
     }
